5ec7019b853c33d79b41c16e931419d98909d2ac
xnasero <xnasero@protonmail.com>
2020-08-02 13:17:26 -0300

    initial commit

---
+++ bboost/bash/nyan/colors.bash
@@ -0,0 +1,10 @@
+# ooh pretty color constants
+declare -r clear=$'\033[H\033[2J'
+declare -r grey=$'\033[38;2;100;100;100m'
+declare -r white=$'\033[38;2;255;255;255m'
+declare -r green=$'\033[38;2;0;255;0m'
+declare -r gold=$'\033[38;2;255;145;0m'
+declare -r red=$'\033[38;2;255;0;0m'
+declare -r answercolor=$'\033[38;2;100;100;100m'
+declare -r reset=$'\033[0m'
+
