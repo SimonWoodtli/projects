9d221138c318c2c41b2301e6df9c9c2c5e88aeee
xnasero <xnasero@protonmail.com>
2020-08-08 20:05:15 -0300

    finished 1st side-exercise: leap

--- exercism/python/leap/leap.py
+++ exercism/python/leap/leap.py
@@ -1,2 +1,18 @@
+#!/usr/bin/python3
+## sweet solution from akostic-kostile:
 def leap_year(year):
-    pass
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+## my solution:
+#def leap_year(year):
+#    if year % 100 == 0 and year % 400 == 0 and year % 4 == 0:
+#        return True
+#    elif year % 4 == 0:
+#        if year % 100 == 0:
+#            return False
+#        return True
+#    else:
+#        return False
+
+
+
