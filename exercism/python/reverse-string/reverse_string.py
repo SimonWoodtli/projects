150a8d3e72ab907ad96fea887b7c6b9d231b2c12
xnasero <xnasero@protonmail.com>
2020-08-10 20:30:56 -0300

    finished 2nd side-exercise: reverse-string

--- exercism/python/reverse-string/reverse_string.py
+++ exercism/python/reverse-string/reverse_string.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python3
+## Solution 1
 def reverse(text):
-    pass
+    opposite=[]
+    for char in text:
+        opposite.append(char)
+    opposite.reverse()
+    return "".join(opposite)
+
+print(reverse("hello"))
+
+## Solution 2
+def reverse(text):
+    return text[::-1]
