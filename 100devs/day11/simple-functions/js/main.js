46738104f15703bbdc2686c47a5e5c8af3883f08
xnasero <xnasero@protonmail.com>
2020-12-11 11:48:24 -0300

    feat: finish bootcamp day11/ exercise

---
+++ 100devs/day11/simple-functions/js/main.js
@@ -0,0 +1,14 @@
+//create a function that adds two numbers and alerts the sum
+function addTwoNumbers(n1,n2) {
+  alert(n1+n2)
+}
+
+//create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(n1,n2,n3) {
+  console.log(n1*n2*n3)
+}
+
+//create a function that divides two numbers and returns the ???
+function returnDivisor(n1,n2) {
+  return n1/n2
+}
