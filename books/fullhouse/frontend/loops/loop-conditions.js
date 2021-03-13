8bd8ae9b5cfad8b5fab947e515428d21455281a4
xnasero <xnasero@protonmail.com>
2021-03-12 22:25:24 -0300

    feat: finish fullhouse loops/ exercises

---
+++ books/fullhouse/frontend/loops/loop-conditions.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const main = document.querySelector('main');
+const randomNumber = getRandomNumber(10);
+let guess;
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
